f226d7963a11463263592617362cd2839c1ebacc
Nick Conklin <nconkl1@outlook.com>
2018-02-13 22:17:17 -0500

    Added listener events for all buttons

--- src/App.java
+++ src/App.java
@@ -2,9 +2,12 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 import javax.sql.DataSource;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.xml.transform.Result;
 import java.sql.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 import static javax.swing.JOptionPane.WARNING_MESSAGE;
 
@@ -27,12 +30,37 @@ public class App {
     private JButton btnClearSQLCommand;
     private JButton btnClearTable;
     private JScrollPane scrlExecution;
+    private JTable tblOutput;
 
     public Connection connection;
+    public ResultSet rs;
+    public boolean connectedToDatabase = false;
+
+    public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
+        ResultSetMetaData metaData = rs.getMetaData();
+
+        // names of columns
+        Vector<String> columnNames = new Vector<String>();
+        int columnCount = metaData.getColumnCount();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
+        }
+
+        // data of the table
+        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        while (rs.next()) {
+            Vector<Object> vector = new Vector<Object>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(rs.getObject(columnIndex));
+            }
+            data.add(vector);
+        }
+
+        return new DefaultTableModel(data, columnNames);
+    }
 
     public App() {
-
-        // Set clear for SQL Command Text Area
+        // Create Listener for Clear SQL button
         btnClearSQLCommand.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -40,7 +68,7 @@ public class App {
             }
         });
 
-        // Set clear command for scroll pane
+        // Create Listener for Clear Table button
         btnClearTable.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -48,35 +76,73 @@ public class App {
             }
         });
 
-        // Set execute button
+        // Create Listener for Connect button
         // Load JDBC Driver
         // Establish Connection
         btnConnect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Get username string
                 String username = txtUsername.getText();
+                // Get password string
                 String password = txtPassword.getText();
+                // Get database string
                 String database = (String)dropDatabase.getSelectedItem();
 
+                // Set username, password, database
                 MysqlDataSource dataSource = new MysqlDataSource();
                 dataSource.setUser(username);
                 dataSource.setPassword(password);
                 dataSource.setURL(database);
 
                 try {
+                    // Disable Connect button, drop-downs and text fields upon successful connection
+                    // Otherwise, throw an exception
                     connection = dataSource.getConnection();
                     btnConnect.setEnabled(false);
                     dropDatabase.setEnabled(false);
                     dropDriver.setEnabled(false);
                     txtUsername.setEditable(false);
                     txtPassword.setEditable(false);
+                    connectedToDatabase = true;
                     JOptionPane.showMessageDialog(null, "Database connected!");
                     txtAConnectionStatus.setText(String.format("Connected to %s", database));
                 } catch (SQLException sql) {
                     JOptionPane.showMessageDialog(null,"Error establishing connection",
                             "Error", WARNING_MESSAGE);
-                    System.err.println(sql);
+                    sql.printStackTrace();
                 }
+
+            }
+        });
+
+        // Create Listener for Execute button
+        btnExecute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) throws IllegalStateException {
+                // Check if connected to the database
+                if (!connectedToDatabase) {
+                    JOptionPane.showMessageDialog(null, "Not connected to database.  Please " +
+                            "connect and try again", "Error", WARNING_MESSAGE);
+                    throw new IllegalStateException("Not connected to database");
+                }
+
+                // Set the query to the text in the text area
+                String query = txtASQLStatement.getText();
+
+                try {
+                    // Create the statement
+                    Statement statement = connection.createStatement();
+                    // Execute the query
+                    rs = statement.executeQuery(query);
+                    JTable table = new JTable(buildTableModel(rs));
+                    JOptionPane.showMessageDialog(null, new JScrollPane(table));
+                } catch (SQLException sql) {
+                    JOptionPane.showMessageDialog(null, "Error with query", "Error",
+                            WARNING_MESSAGE);
+                    sql.printStackTrace();
+                }
+
 
             }
         });
