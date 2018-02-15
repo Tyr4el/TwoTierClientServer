f0f98680519b064c34a10b998739aa7515c25988
Nick Conklin <nconkl1@outlook.com>
2018-02-15 12:27:24 -0500

    Added Disconnect button, handled database errors and sql errors

--- src/App.java
+++ src/App.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Vector;
 
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
 import static javax.swing.JOptionPane.WARNING_MESSAGE;
 
 public class App extends JFrame {
@@ -31,23 +32,32 @@ public class App extends JFrame {
     private JButton btnClearTable;
     private JScrollPane scrlExecution;
     private JTable tblOutput;
+    private JButton disconnectButton;
+
+    // Get username string
+    String username = txtUsername.getText();
+    // Get password string
+    String password = txtPassword.getText();
+    // Get database string
+    String database = (String)dropDatabase.getSelectedItem();
 
     public Connection connection;
     public ResultSet rs;
+    public Statement statement;
     public boolean connectedToDatabase = false;
 
     public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
         ResultSetMetaData metaData = rs.getMetaData();
 
         // names of columns
-        Vector<String> columnNames = new Vector<String>();
+        Vector<String> columnNames = new Vector<>();
         int columnCount = metaData.getColumnCount();
         for (int column = 1; column <= columnCount; column++) {
             columnNames.add(metaData.getColumnName(column));
         }
 
         // data of the table
-        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        Vector<Vector<Object>> data = new Vector<>();
         while (rs.next()) {
             Vector<Object> vector = new Vector<Object>();
             for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
@@ -83,11 +93,11 @@ public class App extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Get username string
-                String username = txtUsername.getText();
+                username = txtUsername.getText();
                 // Get password string
-                String password = txtPassword.getText();
+                password = txtPassword.getText();
                 // Get database string
-                String database = (String)dropDatabase.getSelectedItem();
+                database = (String)dropDatabase.getSelectedItem();
 
                 // Set username, password, database
                 MysqlDataSource dataSource = new MysqlDataSource();
@@ -129,19 +139,46 @@ public class App extends JFrame {
 
                 // Set the query to the text in the text area
                 String query = txtASQLStatement.getText();
+                String[] querySplit = query.split(" ", 2);
+                String firstWord = querySplit[0];
 
                 try {
                     // Create the statement
-                    Statement statement = connection.createStatement();
-                    // Execute the query
+                    statement = connection.createStatement();
                     rs = statement.executeQuery(query);
+                    // Create table to be displayed inside the message dialog
                     JTable table = new JTable(buildTableModel(rs));
                     JOptionPane.showMessageDialog(null, new JScrollPane(table));
                 } catch (SQLException sql) {
-                    JOptionPane.showMessageDialog(null, "Error with query", "Error",
+                    JOptionPane.showMessageDialog(null, sql.getLocalizedMessage(), "Error",
                             WARNING_MESSAGE);
                     sql.printStackTrace();
                 }
+            }
+        });
+
+        disconnectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!connectedToDatabase) {
+                    return;
+                }
+                try {
+                    connection.close();
+                } catch (SQLException sql) {
+                    sql.printStackTrace();
+                }
+
+                btnConnect.setEnabled(true);
+                dropDatabase.setEnabled(true);
+                dropDriver.setEnabled(true);
+                txtUsername.setEditable(true);
+                txtPassword.setEditable(true);
+                connectedToDatabase = false;
+
+                txtAConnectionStatus.setText("Not connected");
+                JOptionPane.showMessageDialog(null, "Disconnected from database", "Success",
+                        INFORMATION_MESSAGE);
             }
         });
     }
@@ -150,7 +187,7 @@ public class App extends JFrame {
         JFrame frame = new JFrame("SQL Client GUI");
         frame.setContentPane(new App().pnlMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(928, 600);
+        frame.setSize(980, 600);
         frame.setResizable(false);
         //frame.pack();
         frame.setVisible(true);
